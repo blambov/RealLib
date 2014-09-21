048b703475ad9b4a16413a76e61df752b17b687c
Branimir Lambov <branimir.lambov@datastax.com>
2014-09-21 17:25:32 +0100

    Change license to the permissive Apache 2.0 to allow wider usage.

--- convolution.cpp
+++ convolution.cpp
@@ -1,23 +1,20 @@
 /*
 
   RealLib, a library for efficient exact real computation
-  Copyright (C) 2006  Branimir Lambov <barnie@brics.dk>
+  Copyright (C) 2006 Branimir Lambov
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
+  This library is licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this library except in compliance with the License.
+  You may obtain a copy of the License at
 
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
+    http://www.apache.org/licenses/LICENSE-2.0
 
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-
-*/ 
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
 
 #include "defs.h"
 #include "convolution.h"
